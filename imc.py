082bdd9dc98da2db5d85158b8bbb07726b41ab7a
Enzo Silva <88846076+enzin23@users.noreply.github.com>
2021-11-25 09:16:49 -0300

    Update imc.py

--- imc.py
+++ imc.py
@@ -1,6 +1,6 @@
 def calculoIMC(pesoObtido, alturaObtida):
     resultBMI = pesoObtido / alturaObtida**2
-    print('Seu IMC: {}'.format(resultBMI))
+    print('Seu IMC: {:.2f}'.format(resultBMI))
 
 weight = float(input('Qual o seu peso (em Kg)? '))
 height = float(input('qual a sua altura (em metros)? '))
